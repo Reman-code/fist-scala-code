6cc4efc90bdfc7eda25666856eecf9e1b32c6608
Thilina Madusanka <77828519+Reman-code@users.noreply.github.com>
2021-05-15 11:36:13 +0530

    This is my first scala functions code

---
+++ first-functions.scala
@@ -0,0 +1,30 @@
+object functions extends {
+
+  def convertCToF(x:Double):Double=x*1.8+32; 
+  
+
+  def sphereVolume(x:Double):Double=22*x*x*x*4/(3*7);
+  
+
+  def wholeSaleCost(x:Int):Double={ 
+    if(x<50){
+      24.95*(60/100)+3
+    } 
+    else{
+      24.95*(60/100)+3+(x-50)*0.75;
+    }
+                                        
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    println("Fahrenheit : " , convertCToF(35));
+
+    println("Volume : " , sphereVolume(5));
+
+    println("Whole sale cost : " , wholeSaleCost(60));
+
+  }
+
+}
